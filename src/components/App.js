008b3b1be14926fd0ab0ce1ec6fd98977b1b0aac
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2022-12-31 17:22:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isValid, setIsValid] = useState(false);
+   const clickHandler = () =>{
+      setIsValid(true);
+   };
   return (
     <div id="main">
       // Do not alter the main div
+    {!isValid && <button type="button" onClick = {clickHandler} id="click">Click Me</button>}
+    {isValid && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
